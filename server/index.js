9d56ae32da85a1f1a4d2fe467eb1ffee3fb063bc
Chance Nguyen <chanceknguyen@gmail.com>
2020-10-14 21:42:55 -0500

    implement create operation on server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const db = require('../database');
+const moment = require('moment');
 
 const app = express();
 const port = 3003;
 
 app.use('/', express.static('client/dist'));
+app.use(express.json());
 
+app.post('/moist-air/reviews', (req, res) => {
+  let gameId = req.query.gameId;
+  let userId = req.body.userId;
+  let body = req.body.body;
+  let recommended = req.body.recommended;
+  var idString = '000' + gameId.toString();
+  idString = idString.slice(-3);
+  var userGameId = userId.toString() + idString;
+  var date = moment().format();
+  var reviewData = {
+    gameId,
+    userId,
+    body,
+    recommended,
+    userGameId,
+    date,
+  }
+  console.log('receiving a post request');
+  console.log(reviewData);
+  db.createReview(reviewData)
+    .then((data) => {
+      console.log(data);
+    })
+})
 
 app.get('/moist-air/reviews', (req, res, next) => {
-  let gameID = req.query.gameID;
-  if (gameID === '') { // default to hello kitty
-    gameID = 1;
+  let gameId = req.query.gameId;
+  console.log(req.query);
+  if (gameId === '') { // default to hello kitty
+    gameId = 1;
   }
-  db.getReviews(gameID)
+  db.getReviews(gameId)
     .then((data) => {
       res.send(data);
     })
