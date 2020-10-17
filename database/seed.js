49feab544cf6ce7fbcd12d614aaddf4e20700c95
Chance Nguyen <chanceknguyen@gmail.com>
2020-10-16 22:31:41 -0500

    optimize data generation with memory drainage and generate more records

--- database/seed.js
+++ database/seed.js
@@ -1,87 +1,220 @@
 const fs = require('fs');
-const csvWriter = require('csv-write-stream');
-const writer = csvWriter();
 const faker = require('faker');
 const moment = require('moment');
+const stringify = require('csv-stringify');
 
 var date = moment().format();
 
 const gameGen = () => {
   var counter = 1;
-  writer.pipe(fs.createWriteStream('games.csv'));
-  for (var i = 0; i < 10000000; i++) {
-    writer.write({
-      id: counter++,
-      Title: faker.commerce.productName(),
-      createdAt: date,
-      updatedAt: date
-    })
+  console.log('starting games.csv generation at', moment().format('MMMM Do YYYY, h:mm:ss a'))
+  var stringifier = stringify({
+    header: true,
+    columns: {
+      id: 'id',
+      Title: 'Title',
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  })
+  stringifier.pipe(fs.createWriteStream('games.csv'));
+  var i = 10000000
+  write();
+  function write() {
+    var ok = true;
+    do {
+      i-=1;
+      if ( i === 0 ) {
+        stringifier.write({
+          id: counter++,
+          Title: faker.commerce.productName(),
+          createdAt: date,
+          updatedAt: date
+        })
+        console.log(`created ${counter} records for games.csv at`, moment().format('MMMM Do YYYY, h:mm:ss a'));
+      } else {
+        ok = stringifier.write({
+          id: counter++,
+          Title: faker.commerce.productName(),
+          createdAt: date,
+          updatedAt: date
+        });
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      stringifier.once('drain', write);
+    }
   }
-  console.log('created games.csv');
 }
 
 const userGen = () => {
   var counter = 1;
-  writer.pipe(fs.createWriteStream('users.csv'));
-  for (var i = 0; i < 1000000; i++) {
-    writer.write({
-      id: counter++,
-      createdAt: date,
-      updatedAt: date,
-      Username: faker.internet.userName(),
-      avatar: faker.internet.avatar(),
-      games_owned_count: Math.floor(Math.random() * 100),
-      reviews_count: Math.floor(Math.random() * 50),
-    })
+  console.log('starting users.csv generation at', moment().format('MMMM Do YYYY, h:mm:ss a'))
+  var stringifier = stringify({
+    header: true,
+    columns: {
+      id: 'id',
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+      Username: 'Username',
+      avatar: 'avatar',
+      games_owned_count: 'games_owned_count',
+      reviews_count: 'reviews_count'
+    }
+  })
+  stringifier.pipe(fs.createWriteStream('users.csv'));
+  var i = 10000000
+  write();
+  function write() {
+    var ok = true;
+    do {
+      i-=1;
+      if ( i === 0 ) {
+        stringifier.write({
+          id: counter++,
+          createdAt: date,
+          updatedAt: date,
+          Username: faker.internet.userName(),
+          avatar: faker.internet.avatar(),
+          games_owned_count: Math.floor(Math.random() * 100),
+          reviews_count: Math.floor(Math.random() * 50),
+        })
+        console.log(`created ${counter} records for users.csv at`, moment().format('MMMM Do YYYY, h:mm:ss a'));
+      } else {
+        ok = stringifier.write({
+          id: counter++,
+          createdAt: date,
+          updatedAt: date,
+          Username: faker.internet.userName(),
+          avatar: faker.internet.avatar(),
+          games_owned_count: Math.floor(Math.random() * 100),
+          reviews_count: Math.floor(Math.random() * 50),
+        });
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      stringifier.once('drain', write);
+    }
   }
-  console.log('created users.csv');
 }
 
 const userGameGen = () => {
   var counter = 1;
-  writer.pipe(fs.createWriteStream('userGames.csv'));
-  for (var i = 0; i < 10000000; i++) {
-    writer.write({
-      id: counter++,
-      GameId: Math.floor(Math.random() * 10000000),
-      UserId: Math.floor(Math.random() * 1000000),
-      time_played: Math.floor(Math.random() * 200),
-      purchase_type: (Math.random() > 0.5) ? 'Steam Purchase' : 'Non-Steam Purchase',
-      createdAt: date,
-      updatedAt: date,
-    })
+  console.log('starting userGames.csv generation at', moment().format('MMMM Do YYYY, h:mm:ss a') )
+  var stringifier = stringify({
+    header: true,
+    columns: {
+      id: 'id',
+      GameId: 'GameId',
+      UserId: 'UserId',
+      time_played: 'time_played',
+      purchase_type: 'purchase_type',
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  })
+  stringifier.pipe(fs.createWriteStream('userGames.csv'));
+  var i = 100000000
+  write();
+  function write() {
+    var ok = true;
+    do {
+      i-=1;
+      if ( i === 0 ) {
+        stringifier.write({
+          id: counter++,
+          GameId: Math.floor(Math.random() * 10000000),
+          UserId: Math.floor(Math.random() * 10000000),
+          time_played: Math.floor(Math.random() * 200),
+          purchase_type: (Math.random() > 0.5) ? 'Steam Purchase' : 'Non-Steam Purchase',
+          createdAt: date,
+          updatedAt: date,
+        })
+        console.log(`created ${counter} records for userGames.csv at`, moment().format('MMMM Do YYYY, h:mm:ss a'));
+      } else {
+        ok = stringifier.write({
+          id: counter++,
+          GameId: Math.floor(Math.random() * 10000000),
+          UserId: Math.floor(Math.random() * 10000000),
+          time_played: Math.floor(Math.random() * 200),
+          purchase_type: (Math.random() > 0.5) ? 'Steam Purchase' : 'Non-Steam Purchase',
+          createdAt: date,
+          updatedAt: date,
+        });
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      stringifier.once('drain', write);
+    }
   }
-  console.log('created userGames.csv');
 }
 
 const reviewsGen = () => {
   var counter = 1;
-  var body = faker.lorem.paragraph()
-
-  writer.pipe(fs.createWriteStream('reviews.csv'));
-  for (var i = 0; i < 100; i++) {
-    writer.write({
-      id: counter++,
-      userGameId: Math.floor(Math.random() * 10000000),
-      recommended: (Math.random() > 0.5) ? true : false,
-      body: body,
-      helpful_count: Math.floor(Math.random() * 50),
-      funny_count: Math.floor(Math.random() * 10),
-      comments_count: Math.floor(Math.random() * 10),
-      awards: "{\"Treasure\":1,\"Mind Blown\":0,\"Golden Unicorn\":0,\"Deep Thoughts\":0,\"Heartwarming\":2,\"Hilarious\":3,\"Hot Take\":3,\"Poetry\":2,\"Extra Helpful\":1}",
-      createdAt: date,
-      updatedAt: date
-    })
+  var body = faker.lorem.paragraph();
+  console.log('starting reviews.csv generation at', moment().format('MMMM Do YYYY, h:mm:ss a'))
+  var stringifier = stringify({
+    header: true,
+    columns: {
+      id: 'id',
+      userGameId: 'userGameId',
+      recommended: 'recommended',
+      body: 'body',
+      helpful_count: 'helpful_count',
+      funny_count: 'funny_count',
+      comments_count: 'comments_count',
+      awards: 'awards',
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  })
+  stringifier.pipe(fs.createWriteStream('reviews.csv'));
+  var i = 100000000
+  write();
+  function write() {
+    var ok = true;
+    do {
+      i-=1;
+      if ( i === 0 ) {
+        stringifier.write({
+          id: counter++,
+          userGameId: counter - 1,
+          recommended: (Math.random() > 0.5) ? true : false,
+          body: body,
+          helpful_count: Math.floor(Math.random() * 50),
+          funny_count: Math.floor(Math.random() * 10),
+          comments_count: Math.floor(Math.random() * 10),
+          awards: "{\"Treasure\":1,\"Mind Blown\":0,\"Golden Unicorn\":0,\"Deep Thoughts\":0,\"Heartwarming\":2,\"Hilarious\":3,\"Hot Take\":3,\"Poetry\":2,\"Extra Helpful\":1}",
+          createdAt: date,
+          updatedAt: date
+        })
+        console.log(`created ${counter} records for reviews.csv at`, moment().format('MMMM Do YYYY, h:mm:ss a'));
+      } else {
+        ok = stringifier.write({
+          id: counter++,
+          userGameId: counter - 1,
+          recommended: (Math.random() > 0.5) ? true : false,
+          body: body,
+          helpful_count: Math.floor(Math.random() * 50),
+          funny_count: Math.floor(Math.random() * 10),
+          comments_count: Math.floor(Math.random() * 10),
+          awards: "{\"Treasure\":1,\"Mind Blown\":0,\"Golden Unicorn\":0,\"Deep Thoughts\":0,\"Heartwarming\":2,\"Hilarious\":3,\"Hot Take\":3,\"Poetry\":2,\"Extra Helpful\":1}",
+          createdAt: date,
+          updatedAt: date
+        });
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      stringifier.once('drain', write);
+    }
   }
-  console.log('created reviews.csv');
 }
 
 async function generateAll() {
-  await gameGen();
+  await gameGen()
   await userGen();
   await userGameGen();
   await reviewsGen();
-  writer.end();
 }
 
 generateAll();
